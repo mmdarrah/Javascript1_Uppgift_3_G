7b9841b9c2d14982212bce3abd5c83c385ef41c9
Mohammed Darrh <mmdarrah@hotmail.com>
2018-10-19 12:52:00 +0200

    Third commit

---
+++ uppgift4.js
@@ -0,0 +1,17 @@
+
+
+
+let sfärsRadie = prompt("Lägga till  sfärs radie för att programmet beräkna\nsfärens volymen och arean.")
+
+sumOfvolym = function(){ // En funktion som beräkna sfärens volym
+let volym =  (4 * 3.14 * Math.pow(sfärsRadie,3) / 3)
+return ( "Sfärens volym är " + volym.toFixed(2));
+// OBS!! toFixed(2) för att bara ha två decimal
+}
+sumOfarea = function(){ // En funktion som beräkna sfärens area
+    let area =  4 * 3.14 * Math.pow(sfärsRadie,2) 
+    return ("Och sfärens area är " + area.toFixed(2));
+// OBS!! toFixed(2) för att bara ha två decimal
+}
+alert(sumOfvolym());
+alert(sumOfarea());
